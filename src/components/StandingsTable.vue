010a615adbb79e1ce8d15ef3837c885b9fd43dca
bengoodheart <bgoodheart@fordham.edu>
2022-09-02 18:33:47 -0400

    added numerical record and win percentage

--- src/components/StandingsTable.vue
+++ src/components/StandingsTable.vue
@@ -17,8 +17,7 @@
         evariant="primary"
       >
         <b-button block v-b-toggle.accordian-2 variant="primary"
-          >NL East Standings</b-button
-        >
+          >NL East Standings</b-button>
       </b-card-header>
       <b-collapse id="accordian-2" accordian="my-accordian" role="tabpanel">
         <b-table :items="NLEastTable.current_table"></b-table>
